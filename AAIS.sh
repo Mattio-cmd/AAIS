bec0ac3754e29e4ef866a66b7b81312119406080
MattioC <coghiqm8@gmail.com>
2024-11-19 11:54:04 -0600

    Added stuff

--- AAIS.sh
+++ AAIS.sh
@@ -1,5 +1,4 @@
 echo "Hello there, your Auto Install should beging shortly"
-echo "btw, im assuming i already installed nvim so yeah, install that if you didnt earlier"
 sleep 3
 echo "Im going to su so we can create the doas.conf, because sudo sucks"
 sleep 1
@@ -30,44 +29,21 @@ Include = /etc/pacman.d/mirrorlist-arch
 
 [multilib]
 Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
+
 echo "Now just check the pacman.conf IN CASE something went wrong"
 doas nvim /etc/pacman.conf
-echo "After that, we download important packages from the Arch Repositories"
-sleep 5
+echo "Downloading important packages from the Arch Repositories..."
+sleep 2
 doas pacman -Syy
 doas pacman -S networkmanager-runit xorg doas xorg-xinit git zsh nitrogen neovim exa dust duf ripgrep zsh-syntax-highlighting xdotool libreoffice-fresh htop pcmanfm cmus mpv sxiv xfce4-screenshoter librewolf
 echo "Now we need to pull dwm and st, my configs basically (also .zshrc and .vimrc)."
-sleep 5
-cd $HOME/.config
-git clone https://github.com/Mattio-cmd/dwm-config-green
-git clone https://github.com/Mattio-cmd/my-st-build
-git clone https://github.com/Mattio-cmd/slock
-git clone https://github.com/Mattio-cmd/slstatus
-git clone https://github.com/Mattio-cmd/x-related-stuff
-git clone https://github.com/Mattio-cmd/my-zshrc
-git clone https://github.com/Mattio-cmd/nvim-lua
-mv my-zshrc/.zshrc $HOME/
-rm -rf my-zshrc
-mv dwm-config-green dwm-6.2
-mv my-st-build st
-echo "Idk if i did it right, do it manually later"
 sleep 3
+cd $HOME/
+echo "I am now going to exit root and go back to a normal user with 'exit'"
+exit
+sleep 2
+
+git clone https://Mattio-cmd/dotfiles
+cd dotfiles; ./install.sh
+
 doas chsh -s /usr/bin/zsh mattio
-cd ~/
-cd $HOME/.config
-echo "Lets build dwm!"
-sleep 2
-cd $HOME/.config/dwm-6.2
-doas make clean install
-cd $HOME/.config/st
-doas make clean install
-cd $HOME/.config/slock
-doas make clean install
-cd $HOME/.config/slstatus
-doas make clean install
-cp $HOME/.config/x-related-stuff/xinit $HOME/.
-mv xinit .xinitrc;
-echo "I think everything is good now, just run startx"
-echo "Removing File System"
-sleep 2
-echo "nah jk"
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AAIS
-AAIS is short for "Arch Auto Install Script" yes i know, really original
-- I made this for myself, but feel free using it. 
+AAIS is short for "Artix Auto Install Script" yes i know, really original
+- I made this for myself, but feel free using it.
 
 # Installation
 There is nothing really to it, just clone the repo and run the script
